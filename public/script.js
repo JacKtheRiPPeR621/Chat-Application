d406cee25383b93e8ebcc5f8bed4ccd07cecabc8
JacKtheRiPPeR621 <78647974+JacKtheRiPPeR621@users.noreply.github.com>
2021-08-02 20:54:32 +0530

    username and password section complete

--- public/script.js
+++ public/script.js
@@ -1,1 +1,32 @@
 let socket = io()
+
+$('#loginBox').show()
+$('#chatBox').hide()
+
+$('#btnStart').click(() => {
+    socket.emit('login' , {
+        username: $('#inpUsername').val(),
+        password: $('#inpPassword').val()
+    })
+})
+
+socket.on('logged_in', () => {
+    $('#loginBox').hide()
+    $('#chatBox').show()
+})
+
+socket.on('login_failed', () => {
+    $('#loginBox').hide()
+    $('#chatBox').show()
+})
+
+$('#btnSendMsg').click(() => {
+    socket.emit('msg_send' , {
+        to: $('#inpToUser').val() ,
+        msg : $('#inpNewMsg').val()
+    })
+})
+
+socket.on('msg_rcvd' , (data)=> {
+    $('#ulMsgs').append($('<li>').text(data.msg))
+})
--- server.js
+++ server.js
@@ -6,9 +6,40 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+let users = {
+    'vivek' : '123'
+}
+
 io.on('connection' , (socket) => {
     console.log('connected with socket id: ', socket.id)
+
+    socket.on('login' , (data) => {
+        if(users[data.username]) {
+            if(users[data.username] == data.password) {
+                socket.join(data.username)
+                socket.emit('logged_in' )
+            }else{
+                socket.emit('login_failed')
+            }
+        }else{
+            users[data.username] = data.password
+            socket.join(data.username)
+            socket.emit('logged_in')
+        }
+        console.log(users)
+    })
+
+    socket.on('msg_send' ,(data) => {
+        if(data.to) {
+            io.to(data.to).emit('msg_rcvd' , data)
+        }else{
+            socket.broadcast.emit('msg_rcvd' , data)
+        }
+    })
+
 })
+
+
 
 app.use('/' , express.static(__dirname + '/public'))
 
